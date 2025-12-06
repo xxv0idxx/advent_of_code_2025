4b5822e9296febb283547b02b84c40978716aefb
xxv0idxx <maia.malone1@gmail.com>
2025-12-05 22:55:42 -0500

    I was thinking maybe I just made the negative check tooo restrictive, so I moved the check to where it actually matter and I believe this is correct,  but now I am picking up extras...

--- python/day_01/main.py
+++ python/day_01/main.py
@@ -50,9 +50,9 @@ def part_2():
             active_val = val
             extra_answers = 0
             #edge case where we had zero last time!!!!!
-            if 0 > val and not prev_hit:#tracking if we are negative
+            if 0 > val:#tracking if we are negative
                 active_val = abs(val) #copy the absolute value of the of the current value to play with
-                if 99 > active_val:
+                if 99 > active_val and not prev_hit:
                     extra_answers = 1 #make sure to track the spin past 0 incase it is only 1 spin and the value remains unders 99 as an absolute
             if active_val > 99: #the lock spins past "0" an amt_spin of times rather than just once
                 # if prev_val > val: #we became negative
