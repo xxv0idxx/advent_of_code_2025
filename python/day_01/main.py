69df4b63015d85c0684866ae7b33a7f137353dab
xxv0idxx <maia.malone1@gmail.com>
2025-12-05 22:30:57 -0500

    first potential solution --checking for negative number under 99 and adding +1 spin, then converting to absolute and if the new val is over 99, checking for new val / 100 to see how many 100s exist within it i.e how many spins it has done, since it could be any amount greater than 1, or 1 if it is just 120, as opposed to -120 which would be too spins, since the last value is always a positive number between 0 and 99

--- python/day_01/main.py
+++ python/day_01/main.py
@@ -34,7 +34,7 @@ def part_2():
 
     with open("test_input.txt") as f:
         for index, line in enumerate(f):
-            spin = False
+            negative = False
             print(f"{index} entry, current value is {val}, current input is {line}")
             direction = line[0]
             print(f"This is the direction {direction}")
@@ -42,26 +42,25 @@ def part_2():
             print(f"This is the amt {amt}")
             match direction:
                 case "L":
-                    if amt > val:
-                        spin = True
                     val = val - amt
                 case "R":
-                    if val + amt > 99:
-                        spin = True
                     val = val + amt
                 case _:
                     print("fuck")
             print(f"This is the val after altering {val}")
-            if spin:
-                extra_answers = 0
-                if prev_val > val: #we became negative
-
-                else: #we went above 99
-
-                extra_answers = abs(val // 100)
+            active_val = val
+            extra_answers = 0
+            if 0 > val:#tracking if we are negative
+                active_val = abs(val) #copy the absolute value of the of the current value to play with
+                extra_answers = 1 #make sure to track the spin past 0 incase it is only 1 spin and the value remains unders 99 as an absolute
+            if active_val > 99: #the lock spins past "0" an amt_spin of times rather than just once
+                # if prev_val > val: #we became negative
+                #     amt_spin = abs(val) - prev_val
+                # else: #we went above 99
+                #     amt_spin = val - prev_val
+                extra_answers = active_val // 100
                 answer = answer + extra_answers
                 print(f"We hit 0 this many times while rotating: {extra_answers}")
-            prev_val = val
             val = val % 100
             print(f"This is the finalized value {val}")
             match val:
@@ -73,7 +72,7 @@ def part_2():
     print(answer)
 
 def main():
-    part_2()
+    part_1()
 
 if __name__ == "__main__":
     main()
