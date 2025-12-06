a231c94ef2bea65a3e742135e3f496267c512ae4
xxv0idxx <maia.malone1@gmail.com>
2025-12-05 23:12:19 -0500

    I thought maybe we were counting extras if it was going to end up hitting, but I think im forgetting how to divide lol

--- python/day_01/main.py
+++ python/day_01/main.py
@@ -52,7 +52,7 @@ def part_2():
             #edge case where we had zero last time!!!!!
             if 0 > val:#tracking if we are negative
                 active_val = abs(val) #copy the absolute value of the of the current value to play with
-                if 99 > active_val and not prev_hit:
+                if 99 >= active_val and not prev_hit:
                     extra_answers = 1 #make sure to track the spin past 0 incase it is only 1 spin and the value remains unders 99 as an absolute
             if active_val > 99: #the lock spins past "0" an amt_spin of times rather than just once
                 # if prev_val > val: #we became negative
@@ -60,6 +60,8 @@ def part_2():
                 # else: #we went above 99
                 #     amt_spin = val - prev_val
                 extra_answers = active_val // 100
+                if val % 100 == 0 and extra_answers > 0:
+                    extra_answers = extra_answers - 1
             print(f"We hit 0 this many times while rotating: {extra_answers}")
             answer = answer + extra_answers
             val = val % 100
