303a80102c8d0810d606a96f45f652d74b05a53f
xxv0idxx <maia.malone1@gmail.com>
2025-12-12 14:18:54 -0500

    Day 2 part 1 complete! pretty fast but the input is tiny lol

--- python/day_02/main.py
+++ python/day_02/main.py
@@ -1,5 +1,23 @@
+def part_1():
+    answer = 0
+    with open("input.txt") as f:
+       content = f.read()
+       list = content.split(",")
+       for i in list:
+           id_min = int(i.split("-")[0])
+           id_max = int(i.split("-")[1])
+           for j in range(id_min,id_max+1):
+               str_j = str(j)
+               str_length = len(str_j)
+               if str_length %2 == 0:
+                   midpoint = int(str_length / 2)
+                   if str_j[:midpoint] == str_j[midpoint:]:
+                        answer = answer + j
+
+    print(answer)
+
 def main():
-    print("Hello from day-02!")
+    part_1()
 
 
 if __name__ == "__main__":
